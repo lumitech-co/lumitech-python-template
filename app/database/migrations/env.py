ccf014f0cc252d964cccc9bf94b6fba71a252aae
Yaroslav Farkash <farkashyaroslav@gmail.com>
2024-12-06 11:15:10 +0200

    feat: configure alembic migration tool

---
+++ app/database/migrations/env.py
@@ -0,0 +1,101 @@
+import asyncio
+from collections.abc import MutableMapping
+from logging.config import fileConfig
+from typing import Literal
+
+from alembic import context
+from sqlalchemy.ext.asyncio.engine import create_async_engine
+from sqlalchemy.future import Connection
+
+from app.database.engine import metadata
+from app.database.models import *  # noqa: F403
+from app.settings import settings
+
+config = context.config
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+target_metadata = metadata
+
+
+def include_name_filter(
+    name: str | None,
+    type_: Literal[
+        "schema",
+        "table",
+        "column",
+        "index",
+        "unique_constraint",
+        "foreign_key_constraint",
+    ],
+    parent_names: MutableMapping[  # noqa: ARG001
+        Literal[
+            "schema_name",
+            "table_name",
+            "schema_qualified_table_name",
+        ],
+        str | None,
+    ],
+) -> bool:
+    if type_ == "schema":
+        return name == target_metadata.schema
+    return True
+
+
+async def run_migrations_offline() -> None:  # noqa: RUF029
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    context.configure(
+        url=settings.db_url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def do_run_migrations(connection: Connection) -> None:
+    """Run actual sync migrations.
+
+    :param connection: connection to the database.
+    """
+    context.configure(
+        connection=connection,
+        target_metadata=target_metadata,
+        version_table_schema=target_metadata.schema,
+        include_schemas=True,
+        include_name=include_name_filter,
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_migrations_online() -> None:
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+    """
+    connectable = create_async_engine(settings.db_url)
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+
+loop = asyncio.get_event_loop()
+task = run_migrations_offline() if context.is_offline_mode() else run_migrations_online()
+
+loop.run_until_complete(task)
