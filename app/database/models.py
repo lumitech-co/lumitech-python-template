c71f77fd08a2ea53c67f1374bbd9e9514edf6a76
Yaroslav Farkash <farkashyaroslav@gmail.com>
2024-12-06 12:37:36 +0200

    feat: add user table, make migration

---
+++ app/database/models.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from sqlalchemy import DateTime, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database.engine import Base
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    email: Mapped[str] = mapped_column(String(100), unique=True, index=True)
+    password: Mapped[str] = mapped_column(String(100), nullable=False)
