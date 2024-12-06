e08e4745361561d82ec84eda76de9bb41fc0e1e8
Yaroslav Farkash <farkashyaroslav@gmail.com>
2024-12-06 12:45:11 +0200

    feat: configure main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,26 @@
+import logging
 from collections.abc import AsyncIterator
 from contextlib import asynccontextmanager
+from typing import Any
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.openapi.utils import get_openapi
 
 from app.database.engine import session_manager
+from app.routes.misc import router as router_misc
+from app.routes.user import router as router_user
+from app.settings import settings
+
+logging.basicConfig(
+    level=settings.log_level.upper(),
+    format="%(levelname)-9s %(asctime)-15s [%(name)s] %(filename)s/%(funcName)s: %(message)s",
+)
+
+ROUTERS = (
+    router_user,
+    router_misc,
+)
 
 
 @asynccontextmanager
@@ -12,4 +29,35 @@ async def lifespan(app: FastAPI) -> AsyncIterator[None]:  # noqa: ARG001
     await session_manager.close_connection()
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(lifespan=lifespan, docs_url=None, redoc_url=None)
+
+for router in ROUTERS:
+    app.include_router(router)
+
+
+def _openapi_schema() -> dict[str, Any]:
+    if app.openapi_schema:
+        return app.openapi_schema
+
+    title = "FastAPI"
+    version = "1.0.0"
+
+    openapi_schema = get_openapi(title=title, version=version, routes=app.routes)
+    openapi_schema["info"] = {"title": title, "version": version}
+
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = _openapi_schema  # type: ignore[method-assign]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        settings.backend_host_url,
+        settings.frontend_host_url,
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
--- app/settings.py
+++ app/settings.py
@@ -4,6 +4,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="allow")
 
+    env: str
+    log_level: str
     backend_host_url: str
     frontend_host_url: str
 
