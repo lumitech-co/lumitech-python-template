2cf4e47a1eababfdb30ca65ea38d602371959bc4
Yaroslav Farkash <farkashyaroslav@gmail.com>
2025-04-11 14:35:35 +0300

    refactor: user endpoints

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,13 +1,14 @@
-from collections.abc import Sequence
+from typing import Any
 
 from fastapi import APIRouter, Response, status
-from pydantic import NonNegativeInt
 
 from app.database.models import User
 from app.manager.user import user_manager
 from app.routes.dependencies import DatabaseSessionDependency
 from app.schemas.user import UserCreate, UserRead, UserUpdate
 from app.utils.constants import DEFAULT_DESC, DEFAULT_ORDER_BY
+from app.utils.pagination import Page
+from app.utils.types import OrderByQuery, UserIdPath
 
 router = APIRouter(tags=["Users"], prefix="/users")
 
@@ -18,24 +19,19 @@ router = APIRouter(tags=["Users"], prefix="/users")
     responses={
         400: {
             "description": "Bad Request Error",
-            "content": {"application/json": {"example": {"detail": "Bad request"}}},
+            "content": {"application/json": {"example": {"detail": "'Bad request' or 'Invalid value for 'orderBy'"}}},
         },
     },
-    response_model=list[UserRead],
+    response_model=Page[UserRead],
 )
 async def fetch_users(
-    *,
-    order_by: str = DEFAULT_ORDER_BY,
-    desc: bool = DEFAULT_DESC,
-    offset: NonNegativeInt | None = None,
-    limit: NonNegativeInt | None = None,
-    session: DatabaseSessionDependency,
-) -> Sequence[User] | None:
-    return await user_manager.fetch(order_by=order_by, desc=desc, offset=offset, limit=limit, session=session)
+    *, order_by: OrderByQuery = DEFAULT_ORDER_BY, desc: bool = DEFAULT_DESC, session: DatabaseSessionDependency
+) -> Any | None:
+    return await user_manager.fetch(order_by=order_by, desc=desc, session=session)
 
 
 @router.get(
-    "/{user_id}",
+    "/{userId}",
     status_code=status.HTTP_200_OK,
     responses={
         400: {
@@ -49,20 +45,17 @@ async def fetch_users(
     },
     response_model=UserRead,
 )
-async def fetch_user(user_id: int, session: DatabaseSessionDependency) -> User:
+async def fetch_user(user_id: UserIdPath, session: DatabaseSessionDependency) -> User:
     return await user_manager.fetch_one(id=user_id, session=session)
 
 
-@router.post(
-    "/create",
-    response_model=UserRead,
-)
+@router.post("", response_model=UserRead)
 async def create_user(user_create: UserCreate, session: DatabaseSessionDependency) -> User:
     return await user_manager.create(user_create, session)
 
 
 @router.patch(
-    "/{user_id}",
+    "/{userId}",
     status_code=status.HTTP_200_OK,
     responses={
         404: {
@@ -72,12 +65,12 @@ async def create_user(user_create: UserCreate, session: DatabaseSessionDependenc
     },
     response_model=UserRead,
 )
-async def update_user(user_id: int, user_update: UserUpdate, session: DatabaseSessionDependency) -> User:
+async def update_user(user_id: UserIdPath, user_update: UserUpdate, session: DatabaseSessionDependency) -> User:
     return await user_manager.update(user_id, user_update, session)
 
 
 @router.delete(
-    "/{user_id}",
+    "/{userId}",
     status_code=status.HTTP_204_NO_CONTENT,
     responses={
         404: {
@@ -86,5 +79,5 @@ async def update_user(user_id: int, user_update: UserUpdate, session: DatabaseSe
         },
     },
 )
-async def delete_user(user_id: int, session: DatabaseSessionDependency) -> Response:
+async def delete_user(user_id: UserIdPath, session: DatabaseSessionDependency) -> Response:
     return await user_manager.delete(user_id, session)
