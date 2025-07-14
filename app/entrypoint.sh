21c29eecb399cdb0bf2bb9e42aa08f4135e0b4d7
Yaroslav Farkash <farkashyaroslav@gmail.com>
2025-07-14 18:05:57 +0300

    refactor: enhance entrypoint

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,14 +1,18 @@
 #! /usr/bin/env bash
-echo "Waiting for the database initialization... "
-sleep 5;
+
+if [ "$ENV" = "local" ] || [ "$ENV" = "dev" ]; then
+  echo "Waiting for the database initialization... "
+  sleep 5;
+fi
 
 echo "Running migrations... "
 alembic upgrade head
 
 echo "Starting project... "
-
-if [ "$ENV" = "dev" ]; then
+if [ "$ENV" = "local" ]; then
   uvicorn main:app --loop uvloop --reload --log-level debug --use-colors --timeout-graceful-shutdown 5 --host 0.0.0.0 --port 80
-elif [ "$ENV" = "prod" ]; then
-  uvicorn main:app --loop uvloop --log-level info --use-colors --timeout-graceful-shutdown 5 --port 80
+elif [ "$ENV" = "dev" ]; then
+  uvicorn main:app --loop uvloop --reload --log-level debug --timeout-graceful-shutdown 5 --host 0.0.0.0 --port 80
+else
+  uvicorn main:app --loop uvloop --log-level info --timeout-graceful-shutdown 5 --host 0.0.0.0 --port 80
 fi
